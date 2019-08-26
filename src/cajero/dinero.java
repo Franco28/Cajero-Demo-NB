1e6a84dcf9157202c3ee741a34b4efd562ce1662
Franco28 <franco.mato.contin@gmail.com>
2019-08-26 12:09:29 -0300

    first

---
+++ src/cajero/dinero.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cajero;
+
+/**
+ *
+ * @author Franco
+ */
+public class dinero {
+    
+    int saldo=150000;
+    int valor_aretirar;
+
+    
+}
